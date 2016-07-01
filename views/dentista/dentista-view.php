312a759c372f0893946b041773fd1f0271d378b2
Francisco Gomes <gomes.tisystem@gmail.com>
2016-07-01 12:07:01 -0300

    Página dentista em andamento

--- views/dentista/dentista-view.php
+++ views/dentista/dentista-view.php
@@ -1,33 +1,10 @@
 	<?php if ( ! defined('ABSPATH')) exit; ?>
 
 	<div class="row">
-
-		<?php
-			if ( $this->logged_in ) 
-			{
-				echo '<p class="alert">Logado</p>';
-			}
-		?>
-
-		<div class="col-md-4"></div>
-		<div class="col-md-4">
-			<?php
-				if( $this->login_error )
-				{
-					echo
-					'
-						<div class="alert alert-info" role="alert">
-			 				 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-			 				 <span aria-hidden="true">&times;</span></button>
-			  				 <strong>Atenção! </strong>'.$this->login_error.'
-						</div>
-
-					';
-
-
-	  			}
-			?>
-
+			<form class="form-signin" method="post">
+		        <h3 class="form-signin-heading"><center><?php echo 'Dentistas cadastrados'; ?></center></h3>
+		        
+      		</form>
 
 		</div>
 		<div class="col-md-4"></div>
