dea5985fbc90924e329a81babcea31e07724678e
Gomes <gomes.tisystem@gmail.com>
2016-08-09 23:55:44 -0300

    Mudanças na página inicial

--- controllers/home-controller.php
+++ controllers/home-controller.php
@@ -1,39 +1,84 @@
 <?php
+
 /**
- * home - Controller de exemplo
+ * UserRegisterController - Controller de exemplo
  *
  * @package OdontoVision
  * @since 0.1
  */
-class HomeController extends MainController
-{
+class HomeController extends MainController {
+    /**
+     * $login_required
+     *
+     * Se a página precisa de login
+     *
+     * @access public
+     */
+    //public $login_required = true;
 
-	/**
-	 * Carrega a página "/views/home/index.php"
-	 */
+    /**
+     * $permission_required
+     *
+     * Permissão necessária
+     *
+     * @access public
+     */
+    //public $permission_required = 'user-register';
+
+    /**
+     * Carrega a página "/views/user-register/index.php"
+     */
     public function index() {
-		// Título da página
-		$this->title = 'Home';
-		
-		// Parametros da função
-		$parametros = ( func_num_args() >= 1 ) ? func_get_arg(0) : array();
-	
-		// Essa página não precisa de modelo (model)
-		
-		/** Carrega os arquivos do view **/
-		
-		// /views/_includes/header.php
+        // Page title
+        $this->title = 'User Register';
+
+        // Verifica se o usuário está logado
+        /* if ( ! $this->logged_in ) {
+
+          // Se não; garante o logout
+          $this->logout();
+
+          // Redireciona para a página de login
+          $this->goto_login();
+
+          // Garante que o script não vai passar daqui
+          return;
+
+          }
+
+          // Verifica se o usuário tem a permissão para acessar essa página
+          if (!$this->check_permissions($this->permission_required, $this->userdata['user_permissions'])) {
+
+          // Exibe uma mensagem
+          echo 'Você não tem permissões para acessar essa página.';
+
+          // Finaliza aqui
+          return;
+          } */
+
+
+
+        // Parametros da função
+        $parametros = ( func_num_args() >= 1 ) ? func_get_arg(0) : array();
+
+        // Carrega o modelo para este view
+        $modelo = $this->load_model('register/register-model');
+
+        /** Carrega os arquivos do view * */
+        // /views/_includes/header.php
         require ABSPATH . '/views/_includes/header.php';
-		
-		// /views/_includes/menu.php
+
+        // /views/_includes/menu.php
         require ABSPATH . '/views/_includes/menu.php';
-		
-		// /views/home/home-view.php
-        require ABSPATH . '/views/home/home-view.php';
-		
-		// /views/_includes/footer.php
+
+        // /views/user-register/index.php
+        require ABSPATH . '/views/register/register-view.php';
+
+        // /views/_includes/footer.php
         require ABSPATH . '/views/_includes/footer.php';
-		
-    } // index
-	
-} // class HomeController
+    }
+
+// index
+}
+
+// class home
