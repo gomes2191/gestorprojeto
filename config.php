7adea305263d0ed513bfd4ea0c137cf9ad229f11
Francisco Aparecido <gomes.tisystem@gmail.com>
2017-03-15 17:11:01 -0300

    Implementado metodo inserção de registro via ajax pagina contas a pagar

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+    /* Configuração geral */
+    // Caminho para a raiz
+    define('ABSPATH', dirname(__FILE__));
+    // Nome do site
+    define('NOME_SITE', 'Odonto C - ');
+    // Idioma do sistema
+    define('LANG', 'pt_BR');
+    // Caminho para a pasta de uploads
+    define('UP_ABSPATH', ABSPATH . '/views/_uploads');
+    // URL da home
+    define('HOME_URI', 'http://localhost/soc');
+    // Nome do host da base de dados
+    define('HOSTNAME', 'localhost');
+    // Nome do banco
+    define('DB_NAME', 'migration_ov');
+    // Usuário do banco
+    define('DB_USER', 'root');
+    // Senha do banco
+    define('DB_PASSWORD', '123456');
+    // Charset da conexão PDO
+    define('DB_CHARSET', 'utf8');
+    // Se você estiver desenvolvendo, modifique o valor para true
+    define('DEBUG', TRUE);
+    /**
+     * Não edite daqui em diante
+     * */
+    // Carrega o loader, que vai carregar a aplicação inteira
+    require_once (ABSPATH . '/loader.php');
