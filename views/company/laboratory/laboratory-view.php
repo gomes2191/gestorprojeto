c3f41c6d579e86b45c47ee0c826a8f4f12b8e5af
Francisco Aparecido <gomes.tisystem@gmail.com>
2017-12-19 13:28:32 -0200

    Melhorias na página laboratório

--- views/company/laboratory/laboratory-view.php
+++ views/company/laboratory/laboratory-view.php
@@ -370,47 +370,8 @@
             </div>
         </div><!-- End modal visualizar -->
         
-        <!-- Modal editar inserir -->
-        <div class="modal fade" id="modalForm" role="dialog">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <!-- Modal Header -->
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal">
-                            <span aria-hidden="true">&times;</span>
-                            <span class="sr-only">Fechar X</span>
-                        </button>
-                        <h4 class="modal-title" id="myModalLabel">Contact Form</h4>
-                    </div>
-
-                    <!-- Modal Body -->
-                    <div class="modal-body">
-                        <p class="statusMsg"></p>
-                        <form class="form" role="form">
-                            <div class="form-group">
-                                <label for="inputName">Name</label>
-                                <input type="text" class="form-control" id="inputName" placeholder="Enter your name"/>
-                            </div>
-                            <div class="form-group">
-                                <label for="inputEmail">Email</label>
-                                <input type="email" class="form-control" id="inputEmail" placeholder="Enter your email"/>
-                            </div>
-                            <div class="form-group">
-                                <label for="inputMessage">Message</label>
-                                <textarea class="form-control" id="inputMessage" placeholder="Enter your message"></textarea>
-                            </div>
-                        </form>
-                    </div>
-
-                    <!-- Modal Footer -->
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
-                        <a href="JavaScript:void(0);" class="btn btn-success" onclick="typeAction( ObjData = {'add'} )">Add User</a>
-                    </div>
-                </div>
-            </div>
-        </div><!--End modal editar inserir-->
         <script>
+            
             // Parâmetros necessários para a requisição Ajax
             var objFinanca = new Financeiro();
             objFinanca.setAjaxData('<?= HOME_URI; ?>/company-laboratory/filters');
@@ -464,5 +425,4 @@
                     }   
                 }
         }
-            
         </script>
