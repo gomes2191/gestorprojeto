77d2599f9585880e4dc6dc506c8b8eec9505ce0f
Francisco Aparecido <gomes.tisystem@gmail.com>
2021-04-18 14:36:26 -0300

    Modificação efetuada em: header.blade.php

--- App/Views/admin/layouts/header.blade.php
+++ App/Views/admin/layouts/header.blade.php
@@ -28,11 +28,11 @@
                             <i class='fal fa-building'></i> EMPRESA
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="dropdown01">
-                            <li><a class="dropdown-item <?= (GFunc::isSite('providers')) ? 'active' : FALSE; ?>" href="{{ constant('Config::HOME_URI') }}/providers">Fornecedores</a></li>
-                            <li><a class="dropdown-item <?= (GFunc::isSite('patrimony')) ? 'active' : FALSE; ?>" href="{{ constant('Config::HOME_URI') }}/patrimony">Patrimônio</a></li>
-                            <li><a class="dropdown-item <?= (GFunc::isSite('stock')) ? 'active' : FALSE; ?>" href="{{ constant('Config::HOME_URI') }}/stock">Controle de Estoque</a></li>
-                            <li><a class="dropdown-item <?= (GFunc::isSite('laboratory')) ? 'active' : FALSE; ?>" href="{{ constant('Config::HOME_URI') }}/laboratory">Laboratório</a></li>
-                            <li><a class="dropdown-item <?= (GFunc::isSite('covenant', 'fees')) ? 'active' : FALSE; ?>" href="{{ constant('Config::HOME_URI') }}/covenant">Convênios / Planos</a></li>
+                            <li><a class="dropdown-item <?= (GFunc::isSite('providers')) ? 'active' : FALSE; ?>" href="{{ Config::HOME_URI }}/providers">Fornecedores</a></li>
+                            <li><a class="dropdown-item <?= (GFunc::isSite('patrimony')) ? 'active' : FALSE; ?>" href="{{ Config::HOME_URI }}/patrimony">Patrimônio</a></li>
+                            <li><a class="dropdown-item <?= (GFunc::isSite('stock')) ? 'active' : FALSE; ?>" href="{{ Config::HOME_URI }}/stock">Controle de Estoque</a></li>
+                            <li><a class="dropdown-item <?= (GFunc::isSite('laboratory')) ? 'active' : FALSE; ?>" href="{{ Config::HOME_URI }}/laboratory">Laboratório</a></li>
+                            <li><a class="dropdown-item <?= (GFunc::isSite('covenant', 'fees')) ? 'active' : FALSE; ?>" href="{{ Config::HOME_URI }}/covenant">Convênios / Planos</a></li>
                             <li><a class="dropdown-item" href="#">Tabela de Honorários</a></li>
                         </ul>
                     </li>
@@ -41,11 +41,11 @@
                             <i class='fal fa-analytics'></i> FINANÇAS
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item <?= (GFunc::isSite('pay')) ? 'active' : FALSE; ?>" href="{{ constant('Config::HOME_URI') }}/pay">Contas a Pagar</a>
-                            <a class="dropdown-item <?= (GFunc::isSite('receive')) ? 'active' : FALSE; ?>" href="{{ constant('Config::HOME_URI') }}/receive">Contas a Receber</a>
-                            <a class="dropdown-item" href="{{ constant('Config::HOME_URI') }}/finances-flow">Fluxo de caixa</a>
-                            <a class="dropdown-item" href="{{ constant('Config::HOME_URI') }}/finances-checks">Controle de Cheques</a>
-                            <a class="dropdown-item" href="{{ constant('Config::HOME_URI') }}/finances-payments">Pagamentos</a>
+                            <a class="dropdown-item <?= (GFunc::isSite('pay')) ? 'active' : FALSE; ?>" href="{{ Config::HOME_URI }}/pay">Contas a Pagar</a>
+                            <a class="dropdown-item <?= (GFunc::isSite('receive')) ? 'active' : FALSE; ?>" href="{{ Config::HOME_URI }}/receive">Contas a Receber</a>
+                            <a class="dropdown-item" href="{{ Config::HOME_URI }}/finances-flow">Fluxo de caixa</a>
+                            <a class="dropdown-item" href="{{ Config::HOME_URI }}/finances-checks">Controle de Cheques</a>
+                            <a class="dropdown-item" href="{{ Config::HOME_URI }}/finances-payments">Pagamentos</a>
                         </div>
                     </li>
                     <li class="nav-item dropdown">
@@ -53,13 +53,13 @@
                             <i class="fal fa-users-cog"></i> USUÁRIOS
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="{{ constant('Config::HOME_URI') }}/users/register-dentist">Inserir Dentista</a>
-                            <a class="dropdown-item" href="{{ constant('Config::HOME_URI') }}/users/register-employee">Inserir Funcionario</a>
-                            <a class="dropdown-item" href="{{ constant('Config::HOME_URI') }}/users">Listar cadastros</a>
+                            <a class="dropdown-item" href="{{ Config::HOME_URI }}/users/register-dentist">Inserir Dentista</a>
+                            <a class="dropdown-item" href="{{ Config::HOME_URI }}/users/register-employee">Inserir Funcionario</a>
+                            <a class="dropdown-item" href="{{ Config::HOME_URI }}/users">Listar cadastros</a>
                         </div>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ constant('Config::HOME_URI') }}/patient-control" title="Gerenciar pacientes no sistema">
+                        <a class="nav-link" href="{{ Config::HOME_URI }}/patient-control" title="Gerenciar pacientes no sistema">
                             <i class="fal fa-hands-usd"></i> CLIENTES
                         </a>
                     </li>
